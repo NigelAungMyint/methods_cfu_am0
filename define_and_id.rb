9845e9f47cac24bff09455de0904772a83d7908a
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-09-15 12:41:22 -0500

    Initial commit

--- define_and_id.rb
+++ define_and_id.rb
@@ -9,19 +9,22 @@
 
 # YOU DO the rest:
 
-
+# The include method is called on the string "Hello World" with the argument "Hello" checking to see if the argument is
+# included in self
+# Will return true because Hello is within Hello World
 "Hello World".include?("Hello")
 
-
+# The end with method is called on the string "Hello World" with the argument "Hello" checking to see if self ends with
+# hello
+# Should return false as Hello World begins with Hello not ends
 "Hello World".end_with?("Hello")
 
-
+#The end with method is called again with a different argument "rld"
+# Should return true since "Hello World" Does indeed end with rld because there are no string characters after d
 "Hello World".end_with?("rld")
 
-
+# The even method is called on the integer 12 and should return true since 12 is an even integer
 12.even?
 
-
+# The next method is called on the integer 18 and should return 19 the next integer sequentially
 18.next
-
-
